3fb829d92d896e543b835f74db3349b6767e5ab3
zhaoyijun1551 <zhj1551@163.com>
2020-12-27 21:55:18 +0800

    3.mall整合Redis的过程，以短信验证码的存储验证为例。

---
+++ src/main/java/com/macro/mall/tiny/service/UmsMemberService.java
@@ -0,0 +1,15 @@
+package com.macro.mall.tiny.service;
+
+import com.macro.mall.tiny.common.api.CommonResult;
+
+public interface UmsMemberService {
+    /**
+     * 生成验证码
+     */
+    CommonResult generateAuthCode(String telephone);
+
+    /**
+     * 判断验证码和手机号码是否匹配
+     */
+    CommonResult verifyAuthCode(String telephone, String authCode);
+}
